217c81003f85a54fe0e41d999946a92feb4f8d07
Vlad Ageysha <ander123fakk@gmail.com>
2022-10-27 19:52:29 +0300

    Add library for svg animate and svg icon for x and o

--- src/components/Cell/index.js
+++ src/components/Cell/index.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './index.css';
 
+import Vivus from 'vivus';
+import svgIcon from './x.svg';
+
 const Cell = () => {
+    function handleClick(e) {
+        const cell = e.currentTarget;
+
+        // cell.innerText = 'X';
+        new Vivus(
+            cell,
+            {
+                type: 'delayed',
+                duration: 40,
+                file: svgIcon,
+                animTimingFunction: Vivus.EASE
+            },
+        );
+    }
+
+    
+    
     return(
-        <div className='cell'></div>
+        <div className='cell' onClick={handleClick}></div>
     )
 }
 
