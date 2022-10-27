3ecff571c90fd92cbeff2cd6814ee54db889df8c
Vlad Ageysha <ander123fakk@gmail.com>
2022-10-27 14:37:15 +0300

    Styled field

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import './index.css';
 
-const Field = () => {
-    function handleClick(event) {
-        console.log(event.currentTarget)
-    }
+const Field = () => <div className='field'></div>;
 
-    const cells = [];
-    
-    for (let i = 0; i < 9; i++) {
-        cells.push(<div className='field__cell' onClick={handleClick} key={i}></div>);
-    }
-
-    return(
-        <div className='field'>
-            {cells}
-        </div>
-    )
-}
 
 export default Field;
