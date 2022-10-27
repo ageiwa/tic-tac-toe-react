5bcb649e9a29d7a9a4371ca62c589dfb5246dcb3
Vlad Ageysha <ander123fakk@gmail.com>
2022-10-27 14:52:14 +0300

    Output cells on field

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import './index.css';
 
-const Field = () => <div className='field'></div>;
+import Cell from '../Cell';
 
+const Field = () => {
+    const cells = [];
+
+    for (let i = 0; i < 9; i++) {
+        cells.push(<Cell key={i} />);
+    }
+
+    return(
+        <div className='field'>
+            {cells}
+        </div>
+    )
+}
 
 export default Field;
