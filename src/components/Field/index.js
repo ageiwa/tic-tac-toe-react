69e8b09cd85f46cd01875b8947d240cef257a062
Vlad Ageysha <ander123fakk@gmail.com>
2022-12-09 19:54:35 +0300

    When the user clicks on a cell, its value is passed to the array field

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -7,7 +7,33 @@ import svgX from './x.svg';
 import svgO from './o.svg';
 
 const Field = () => {
+    const [order, setOrder] = useState('X');
+    const [field, setField] = useState([
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ]);
+    
     const cells = [];
+
+    function makeMove(i, j) {
+        const fieldAr = field;
+
+        fieldAr[i][j] = order;
+
+        if (order === 'X') setOrder('O');
+        else setOrder('X');
+
+        setField(fieldAr);
+    }
+
+    for (let i = 0; i < field.length; i++) {
+        
+        for (let j = 0; j < field[i].length; j++) {
+            cells.push(<Cell key={i+''+j} handleClick={() => makeMove(i, j)} />)
+        }
+
+    }
 
     return(
         <div className='field'>
