8279edb569616a9f14c165bc6a8f7d91652b1733
Vlad Ageysha <ander123fakk@gmail.com>
2022-12-09 10:26:30 +0300

    I make click order for X and O

--- src/components/Cell/index.js
+++ src/components/Cell/index.js
@@ -1,29 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
-import Vivus from 'vivus';
-import svgIcon from './x.svg';
-
-const Cell = () => {
-    function handleClick(e) {
-        const cell = e.currentTarget;
-
-        // cell.innerText = 'X';
-        new Vivus(
-            cell,
-            {
-                type: 'delayed',
-                duration: 40,
-                file: svgIcon,
-                animTimingFunction: Vivus.EASE
-            },
-        );
-    }
-
-    
-    
+const Cell = (props) => {
     return(
-        <div className='cell' onClick={handleClick}></div>
+        <div className='cell' onClick={props.handleClick}></div>
     )
 }
 
--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
+
+import Vivus from 'vivus';
+import svgX from './x.svg';
+import svgO from './o.svg';
 
 import Cell from '../Cell';
 
-const Field = () => {
+const Field = (props) => {
+    const [flag, setFlag] = useState(true);
     const cells = [];
 
+    function fun(e) {
+        const cell = e.currentTarget;
+
+        let player = svgX;
+
+        if (flag) {
+            player = svgX;
+            setFlag(false);
+        }
+        else {
+            player = svgO;
+            setFlag(true);
+        }
+
+        new Vivus(
+            cell,
+            {
+                type: 'oneByOne',
+                duration: 40,
+                file: player,
+                animTimingFunction: Vivus.EASE
+            },
+        );
+    }
+
     for (let i = 0; i < 9; i++) {
-        cells.push(<Cell key={i} />);
+        cells.push(<Cell key={i} handleClick={fun} />);
     }
 
     return(
