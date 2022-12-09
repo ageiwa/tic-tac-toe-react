f288bee3df2e40df09becc76590eeeae9bfaba2b
Vlad Ageysha <ander123fakk@gmail.com>
2022-12-09 23:20:12 +0300

    Add icon in cell when player clicked

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -3,6 +3,7 @@ import './index.css';
 
 import Cell from '../Cell';
 
+import Vivus from 'vivus';
 import svgX from './x.svg';
 import svgO from './o.svg';
 
@@ -16,21 +17,41 @@ const Field = () => {
     
     const cells = [];
 
-    function makeMove(i, j) {
+    function makeMove(e, i, j) {
+        const cell = e.target;
         const fieldAr = field;
 
-        fieldAr[i][j] = order;
+        if (fieldAr[i][j] === '') {
+            let player = svgX;
 
-        if (order === 'X') setOrder('O');
-        else setOrder('X');
+            if (order === 'X') {
+                player = svgX;
+                setOrder('O');
+            }
+            else {
+                player = svgO;
+                setOrder('X');
+            }
 
-        setField(fieldAr);
+            fieldAr[i][j] = order;
+            setField(fieldAr);
+
+            new Vivus(
+                cell,
+                {
+                  type: 'oneByOne',
+                  duration: 50,
+                  file: player,
+                  animTimingFunction: Vivus.EASE
+                }
+            );
+        }
     }
 
     for (let i = 0; i < field.length; i++) {
         
         for (let j = 0; j < field[i].length; j++) {
-            cells.push(<Cell key={i+''+j} handleClick={() => makeMove(i, j)} />)
+            cells.push(<Cell key={i+''+j} handleClick={(e) => makeMove(e, i, j)} />)
         }
 
     }
