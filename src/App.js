48b864108a40416668017cc6c41360a53a7c2646
Vlad Ageysha <ander123fakk@gmail.com>
2022-12-12 10:50:37 +0300

    Add btn for restart and title

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Field from './components/Field';
 const App = () => {
   return(
     <div className='tic-tac-toe'>
+      <h1>Ходит игрок: Х</h1>
       <Field />
+      <button className='btn-restart'></button>
     </div>
   );
 }
