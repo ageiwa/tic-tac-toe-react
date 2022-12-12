fe5d32944a49ae5fd518b09bd23aaa96c29723a5
Vlad Ageysha <ander123fakk@gmail.com>
2022-12-12 11:06:09 +0300

    Add svg icons in title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Field from './components/Field';
 
+import svgX from './components/Field/x.svg';
+import svgO from './components/Field/o.svg';
+
 const App = () => {
+  let player = svgX;
+
   return(
     <div className='tic-tac-toe'>
-      <h1>Ходит игрок: Х</h1>
+      <h1 className='title'>
+        Ходит игрок: <img className='title__img' src={player}></img>
+      </h1>
       <Field />
       <button className='btn-restart'></button>
     </div>
