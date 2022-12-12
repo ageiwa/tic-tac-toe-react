e0db372521edc8da5c35b455009fca29a8049b72
Vlad Ageysha <ander123fakk@gmail.com>
2022-12-12 08:54:26 +0300

    Change animaton X from 500 to 250

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -8,7 +8,7 @@ import Snap from 'snapsvg-cjs';
 function animateLine(svg, x1, y1, x2, y2, callback) {
     const line = svg.line(x1, y1, x1, y1);
     line.attr({stroke: '#fff', strokeWidth: 8});
-    line.animate({x1: x1, y1: y1, x2: x2, y2: y2}, 500, callback);
+    line.animate({x1: x1, y1: y1, x2: x2, y2: y2}, 250, callback);
 }
 
 function animateCicle(svg) {
